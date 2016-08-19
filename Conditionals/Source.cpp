7e5a137662aa2aeb4342264a1d6e12ab26fdfb0f
WilsonMiranne <wilsonmiranne1997@gmail.com>
2016-08-19 17:49:54 -0500

    Completed excercises 1 and 2
    ready to move onto 3 at hom

---
+++ Conditionals/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+
+	//1
+	int x;
+	int y=0;
+	if (y == 0)
+	{
+	x = 100;
+	}
+
+	//2
+	int num1=5;
+	int num2=10;
+	if (num1 > num2)
+	{
+		std::cout << num1 << std::endl;
+	}
+	else if (num1 < num2) 
+	{
+		std::cout << num2 << std::endl;
+	}
+	
+	
+	system("pause");
+	return 0;
+}
